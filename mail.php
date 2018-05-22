63decc698942633e94a8f03b4b8d92ad6b76c085
Joshua Ward <jshward069@gmail.com>
2018-05-21 20:58:57 -0700

    Just adding things

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $email 		= $_POST['email'];
 $msg 		= $_POST['msg'];
 $output 	= "Name: ".$name."\n\nEmail: ".$email."\n\nMessage: ".$msg;
 
-$to 		= 'irashad42@gmail.com';
+$to 		= 'jshward069@gmail.com';
 $headers	= 'FROM: "'.$email.'"';
 
 $send		= mail($to, $name, $output."\n\n***This message has been sent from Rokstar", $headers);
